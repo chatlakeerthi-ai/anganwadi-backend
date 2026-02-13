cc20425e32d7f0b2d74e683828a0e5de0676103b
Keerthi <keerthi@gmail.com>
2026-02-13 16:26:30 +0530

    fix table reference error

--- app/init_db.py
+++ app/init_db.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .db import engine
 from .models import Base
+from app.models import child, assessment   # 👈 ADD THIS LINE
 
-
-def init_db() -> None:
+def init_db():
     Base.metadata.create_all(bind=engine)
