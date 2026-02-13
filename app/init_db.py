cf4370758ffede64820293f8054a79251f27dcd6
Keerthi <keerthi@gmail.com>
2026-02-13 16:39:45 +0530

    fix init_db import

--- app/init_db.py
+++ app/init_db.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .db import engine
 from .models import Base
-from app.models import child, assessment   # 👈 ADD THIS LINE
+import app.models
 
 def init_db():
     Base.metadata.create_all(bind=engine)
